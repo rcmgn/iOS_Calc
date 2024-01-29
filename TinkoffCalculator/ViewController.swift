ecf9335877524618aa4011a076bef858d4b8d539
a.dedurin <a.dedurin@tinkoff.ru>
2024-01-29 09:09:31 +0300

    Add calculator buttons and button handlers

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -1,17 +1,22 @@
 //
 //  ViewController.swift
-//  TinkoffCalculator
+//  iOS_Calc
 //
-//  Created by Andrey Dedurin on 29.01.2024.
+//  Created by Andrey Dedurin on 23.01.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let ButtonText = sender.titleLabel?.text else { return }
+        print(ButtonText)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Test")
     }
 
 
